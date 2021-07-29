e99e30b181ee169a3b0baf64e593a3cfb14cbbeb
Charles-Meldhine Madi Mnemoi <63333367+cmmm976@users.noreply.github.com>
2021-07-29 18:38:00 +0300

    script to get lyrics

--- src/getLyrics.py
+++ src/getLyrics.py
@@ -1,5 +1,19 @@
 from lyricsgenius import Genius
 from decouple import config
+import pandas as pd
+
+def downloadLyrics(artist_names, max_songs=None):
+    artist_objects = [api.search_artist(name, max_songs=max_songs) for name in artist_names]
+    for artist in artist_objects:
+        artist.save_lyrics()
+
 
 GENIUS_CLIENT_ACCESS_TOKEN = config('GENIUS_CLIENT_ACCESS_TOKEN')
-print(GENIUS_CLIENT_ACCESS_TOKEN)
+api = Genius(GENIUS_CLIENT_ACCESS_TOKEN)
+api.retries=1000
+api.timeout=60
+api.remove_section_headers=True
+
+artists = pd.read_csv('../data/artists.csv')['artists'].values.tolist()
+
+downloadLyrics(artists)
