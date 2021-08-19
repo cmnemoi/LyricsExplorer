7d2b57a005a4a90132945e31d93ab76391df7e38
Charles-Meldhine Madi Mnemoi <63333367+cmmm976@users.noreply.github.com>
2021-08-19 18:19:44 +0200

    Add files via upload

--- src/getLyrics.py
+++ src/getLyrics.py
@@ -5,23 +5,22 @@ import json
 
 
 client_access_token = config('GENIUS_CLIENT_ACCESS_TOKEN')
-# artists = pd.read_csv('../data/artists.csv')['artists'].values.tolist()
 
 api = genius.Genius(client_access_token)
 api.remove_section_headers=True
 api.retries=1000
-api.timeout=60
+api.timeout=120
 api.excluded_terms = ["(Remix)", "(Live)"]
 
-def getLyrics(artists,max_songs=2):
+def getLyrics(artists,max_songs=None):
     for artist in artists:
         a = api.search_artist(artist,max_songs)
-        a.save_lyrics('test_'+artist,overwrite=True)
+        a.save_lyrics('lyrics_'+artist,overwrite=True)
 
 def getSongs(artists):
     songs = {}
     for artist in artists:
-        songs[artist] = json.load(open('test_'+artist+'.json'))['songs']
+        songs[artist] = json.load(open('lyrics_'+artist+'.json'))['songs']
     return songs
 
 def formatSongs(raw_songs,artists):
@@ -37,7 +36,7 @@ def formatSongs(raw_songs,artists):
        'song_art_secondary_color', 'song_art_text_color', 'primary_artist',
        'apple_music_id', 'apple_music_player_url', 'description',
        'embed_content', 'featured_video', 'lyrics_placeholder_reason',
-       'recording_location', 'release_date', 'release_date_for_display',
+       'recording_location', 'release_date_for_display',
        'current_user_metadata', 'album', 'custom_performances',
        'description_annotation', 'featured_artists',
        'lyrics_marked_complete_by', 'media', 'producer_artists',
@@ -46,9 +45,9 @@ def formatSongs(raw_songs,artists):
     songs.reset_index(inplace=True,drop=True)
     return songs
 
-
-artists = ['MRC','Vald']
-getLyrics(artists)
+artists = pd.read_csv('../artists.csv')['artists'].values.tolist()
+# artists = ['NF','Vald']
+getLyrics(artists[54:len(artists)])
 raw_songs = getSongs(artists)
 songs = formatSongs(raw_songs,artists)
-print(songs)
+songs.to_csv('../songs.csv')
